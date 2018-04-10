d3f1b880831602367db7688906d4f1996533dcf7
jaime <jaime@DESKTOP-1E3KEIM>
2018-04-10 18:28:44 +0200

    añadida añadir comedia y autor

---
+++ LibreriaSD/src/main/java/es/sidelab/LibreriaSD/AutorController.java
@@ -0,0 +1,27 @@
+package es.sidelab.LibreriaSD;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AutorController {
+	
+	@Autowired
+	private AutorRepository repositorioAutor;
+
+	@PostMapping("/LibreriaSD/añadir/autor/solicitud")
+	public String añadirEditorial(@RequestParam String nombre,
+			Model model) {
+
+		Autor autor = new Autor(nombre);
+
+		repositorioAutor.save(autor);
+
+		return "añadirAutor";
+	}
+        
+}
